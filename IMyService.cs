48f9175b4b918ad8b90ec079b1958c5556cc8b39
Debid Yadav <david23n23@gmail.com>
2024-06-21 10:14:52 +0000

    Passing object to function

---
+++ IMyService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestService
+{
+    [ServiceContract]
+    internal interface IMyService
+    {
+        [OperationContract]
+        string getData();
+
+        [OperationContract]
+        string greet(string name);
+
+        [OperationContract]
+        int[] sortArray(int[] ar);
+
+        [OperationContract]
+        int totalMarks(Student s);
+    }
+
+    [DataContract]
+    class Student
+    {
+        [DataMember]
+        public int marks1;
+        [DataMember]
+        public int marks2;
+    }
+}
---
+++ MyService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestService
+{
+    internal class MyService : IMyService
+    {
+        public string getData()
+        {
+            return "Hello World";
+        }
+
+        public string greet(string name)
+        {
+            return "Hello" + name;
+        }
+
+        public int[] sortArray(int[] ar)
+        {
+            Array.Sort(ar);
+            return ar;
+        }
+
+        public int totalMarks(Student s)
+        {
+            return s.marks1 + s.marks2;
+        }
+    }
+}
